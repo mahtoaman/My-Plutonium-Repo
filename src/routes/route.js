31f1f3562792e126ba27281d15221bd6634b271e
mahtoaman <itismyacnt@gmail.com>
2022-08-08 13:28:02 +0530

    External Module  Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const abc = require('../introduction/intro')
+const { result } = require('lodash');
+const lodash = require('lodash');
+// const underScore = require('underscore')
 
+const abc = require('../introduction/intro')
 const wel = require('../logger/logger')
 const helper = require('../util/helper')
 const validator = require('../validator/formatter')
@@ -8,9 +11,9 @@ const validator = require('../validator/formatter')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+
     console.log('My batch is', abc.name)
     abc.printName()
-
     wel.welcome()
     helper.printDate()
     helper.printMonth()
@@ -18,6 +21,40 @@ router.get('/test-me', function (req, res) {
     validator.trimStr()
     validator.upCase()
     validator.lwCase()
+
+    // let weekend = ['Saturday', 'Sunday']
+    // let result = underScore.first(weekend)
+    // console.log('Underscore example is', result)
+
+    //EXTERNAL-MODULE ASSIGNMENT PROBLEM 4
+
+    let array = ['Januray','February','March','April','May','June','July','August','September','October','November','December']
+    const result1 = lodash.chunk(array,4)
+    console.log(result1)
+
+    let array2 = [1,3,5,7,9,11,13,15,17,19]
+    const resutl2 = lodash.tail(array2)
+    console.log(resutl2)
+
+    //Third problem
+
+    let arr1 = [1,2,2]
+    let arr2 = [2,3,3]
+    let arr3 = [3,4,4]
+    let arr4 = [4,5,5]
+    let arr5 = [5,6,6]
+    const UnionResult = lodash.union(arr1,arr2,arr3,arr4,arr5)
+    console.log(UnionResult)
+
+    const pArr1 = [['Fruit','Apple'],
+     ['Vegi','Potato'],
+    ['Tree','BanyanbTree'],
+     ['Plant','Pea Plant']]
+
+    const pairResult = lodash.fromPairs(pArr1)
+    console.log(pairResult)
+
+    
 
     res.send('My second ever api!')
 });
