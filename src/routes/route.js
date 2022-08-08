e18f27ef78b565e9ed70123db2398cd9a81ddc1a
mahtoaman <itismyacnt@gmail.com>
2022-08-08 22:38:42 +0530

    Assignmet on get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,39 +1,135 @@
-const express = require('express');
-const abc = require('../introduction/intro')
+const express = require("express");
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+// 1. Create an API for GET /movies that returns a list of movies. Define an array of
+// movies in your code and return the value in response.
 
-    res.send('My second ever api!')
+router.get("/movies", function (req, res) {
+  let movies = [
+    "Iron Man 3",
+    "Thor Ragnarok",
+    "The Incredible Hulk",
+    "The lord of the rings",
+  ];
+  res.send(movies);
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+// 2. Create an API GET /movies/:indexNumber (For example GET /movies/1 is a
+// valid request and it should return the movie in your array at index 1). You can define an
+// array of movies again in your api
+// [‘Rang de basanti’, ‘The shining’, ‘Lord of the rings’, ‘Batman begins’
 
-router.get('/student-details/:name/:age', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+// 3. Handle a scenario in problem 2 where if the index is greater than the valid
+// maximum value a message is returned that tells the user to use a valid index in an error
+// message.
 
-    let requestParams = req.params
+router.get("/movies/:indexNumber", function (req, res) {
+  let movies = [
+    "Rang de basanti",
+    "The shining",
+    "Lord of the rings",
+    "Batman begins",
+  ];
 
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    let studentAge = requestParams.age
-    console.log('Name of the student is ', studentName, 'and age is: ',studentAge)
-    
-    res.send('Name of the student is ' + studentName + 'and age is: ' + studentAge)
-})
+  let requestParams = req.params;
+  let movieIndex = parseInt(requestParams.indexNumber);
+
+  if (movieIndex >= 0 && movieIndex < movies.length) {
+    const result = movies[movieIndex]; //araray[0]
+    res.send(result);
+  }
+  //solution for problem 3 handling the error
+  else {
+    res.send("Enter a valid number.");
+  }
+});
+
+// 4 Write another api called GET /films. Instead of an array of strings define an array
+// of movie objects this time. Each movie object should have values - id, name. An
+// example of movies array is
+// [ {
+// “id”: 1,
+// “name”: “The Shining”
+// }, {
+// “id”: 2,
+// “name”: “Incendies”
+// }, {
+// “id”: 3,
+// “name”: “Rang de Basanti”
+// }, {
+// “id”: 4,
+// “name”: “Finding Nemo”
+// }]
+// Return the entire array in this api’s response
+
+router.get("/films", function (req, res) {
+  let films = [
+    {
+      id: 1,
+      name: "The Shining",
+    },
+    {
+      id: 2,
+      name: "Incendies",
+    },
+    {
+      id: 3,
+      name: "Rang de Basanti",
+    },
+    {
+      id: 4,
+      name: "Finding Nemo",
+    },
+  ];
+
+  res.send(films);
+});
+
+//5. Write api GET /films/:filmId where filmId is the value received in request path
+// params. Use this value to return a movie object with this id. In case there is no such
+// movie present in the array, return a suitable message in the response body. Example
+// for a request GET /films/3 should return the movie object
+// {
+//     “id”: 3,
+//     “name”: “Rang de Basanti”
+//     }
+//     Similarly for a request GET /films/9 the response can be something like - ‘No
+//     movie exists with this id
+
+router.get("/films/:filmId", function (req, res) {
+  let films = [
+    {
+      id: 1,
+      name: "The Shining",
+    },
+    {
+      id: 2,
+      name: "Incendies",
+    },
+    {
+      id: 3,
+      name: "Rang de Basanti",
+    },
+    {
+      id: 4,
+      name: "Finding Nemo",
+    },
+  ];
+
+  let requestParams = req.params;
+  const filmid = parseInt(requestParams.filmId);
+  const fimlLength = films.length;
+
+  let result = "";
+  for (let i = 0; i < fimlLength; i++) {
+    if (filmid === films[i].id) {
+      result = films[i];
+      break;
+    } else {
+      result = "No movie exists with this id";
+    }
+  }
+  res.send(result);
+});
 
 module.exports = router;
