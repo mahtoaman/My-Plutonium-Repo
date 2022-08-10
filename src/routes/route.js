bef6aadf999f90d8cb52a2c6b4ca51d7b49d9e70
mahtoaman <itismyacnt@gmail.com>
2022-08-10 16:15:11 +0530

    post-api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,50 +1,42 @@
 const express = require("express");
+const router = express.Router();
 
+let players = [
+  {
+    name: "manish",
+    dob: "1/1/1995",
+    gender: "male",
+    city: "jalandhar",
+    sports: ["swimming"],
+  },
+  {
+    name: "gopal",
+    dob: "1/09/1995",
+    gender: "male",
+    city: "delhi",
+    sports: ["soccer"],
+  },
+  {
+    name: "lokesh",
+    dob: "1/1/1990",
+    gender: "male",
+    city: "mumbai",
+    sports: ["soccer"],
+  },
+];
 
+router.post("/players", function (req, res) {
+  //LOGIC WILL COME HERE
+  let newPlayerDetails = req.body
+  let newPlayerName = newPlayerDetails.name
 
-   //1 -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
-
-const router = express.Router();router.get('/sol-1',function(req,res){
-    let arr= [1,2,3,5,6,7]
- 
-    let total = 0;
-   for(let i=0;i<arr.length;i++){
-       total=total+arr[i];
-       
-   }
- const lastDigit=arr.pop()
- 
- 
- const consSum=lastDigit*(lastDigit+1)/2;
- const missingNumber=consSum-total;
-  
-    res.send(`Missing Number is: ${missingNumber}`);
-  });
-
-
-// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
-
-router.get('/sol-2',function(req,res){
-    let arr= [33, 34, 35, 37, 38]
-   let len= arr.length
- 
-   let total = 0;
-   for (let i=0;i<len;i++) {
-       total =total+ arr[i];
-   }
- 
-   let firstDigit= arr[0]
-   let lastDigit= arr.pop()
-   let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
-   let missingNumber= consecutiveSum - total
-  
-    res.send(`Missing Number is: ${missingNumber}`);
-  });
-
+  for(let i=0;i<players.length;i++){
+  if(newPlayerName === players[i].name){
+    return res.send("Player Already Exist")
+  }  
+}
+players.push(newPlayerDetails)
+res.send({data: players, status: true });
+})
 
 module.exports = router;
-
-
-
-
-
