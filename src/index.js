76ff73c730f1f32f62ac9931c6bf58c71e319123
mahtoaman <itismyacnt@gmail.com>
2022-08-24 21:10:10 +0530

    assignemnt/middleware-1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const moment = require('moment')
 const { default: mongoose } = require('mongoose');
 const app = express();
 
@@ -14,12 +15,41 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+
+// let getIP = require('ipware')().get_ip;
+let requestIp = require('request-ip');
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        let today = moment().format('YYYY-MM-DD h:mm:ss');
+        let ip = requestIp.getClientIp(req)
+        // let ip = req.ip
+        console.log(` ${today}, ${ip}, ${req.path}`)
         next();
   }
   );
+
+  
+
+
+
+
+//   var requestIp = require('request-ip');
+
+//   // inside middleware handler
+//   var ipMiddleware = function(req, res, next) {
+//       var clientIp = requestIp.getClientIp(req); // on localhost > 127.0.0.1
+//       next();
+//   };
+
+
+
+
+
+
+
+
+
+
 
 app.use('/', route);
 
